fe4903639bd24873ddf1b57857e13f5b9535209b
Niv Sardi <xaiki@evilgiggle.com>
2016-04-01 14:14:44 -0300

    init
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

---
+++ gulpfile.babel.js
@@ -0,0 +1,45 @@
+import gulp from 'gulp'
+import babel from 'gulp-babel'
+import concat from 'gulp-concat'
+import cleanCSS from 'gulp-clean-css'
+import htmlmin from 'gulp-htmlmin'
+import uglify from 'gulp-uglify'
+import rimraf from 'gulp-rimraf'
+import install from 'gulp-install'
+import zip from 'gulp-zip'
+import runSequence from 'run-sequence'
+
+/* Build Tasks */
+
+gulp.task('build-js', () => gulp.src('src/**/*.js')
+          .pipe(babel())
+          .pipe(uglify())
+          .pipe(gulp.dest('build')))
+
+gulp.task('build-html', () => gulp.src('src/**/*.html')
+          .pipe(htmlmin({ collapseWhitespace: true }))
+          .pipe(gulp.dest('build'))
+)
+
+gulp.task('build-styles', () => {
+    gulp.src('src/css/app/**/*.css')
+        .pipe(cleanCSS())
+        .pipe(concat('app.css'))
+        .pipe(gulp.dest('build/css'))
+
+    return gulp.src('src/vendor/**/*.css')
+        .pipe(cleanCSS())
+        .pipe(concat('vendor.css'))
+        .pipe(gulp.dest('build/css'))
+})
+
+gulp.task('clean-build', () => gulp.src(['build', 'release'], { read: false }).pipe(rimraf()))
+
+gulp.task('zip-release', () => gulp.src('build/**/*')
+    .pipe(zip('bundle.zip'))
+    .pipe(gulp.dest('release')))
+
+
+/* npm tasks */
+
+gulp.task('build', callback => runSequence('clean-build', 'build-js', 'build-html', 'build-styles', 'zip-release', callback))
