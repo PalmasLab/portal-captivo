909bbcd3f7620a05a6a78961be38a73d4bb483cb
Niv Sardi <xaiki@evilgiggle.com>
2016-04-04 10:53:21 -0300

    inline all the things
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,9 @@ import symlink from  'gulp-sym'
 import babel from 'gulp-babel'
 import concat from 'gulp-concat'
 import cleanCSS from 'gulp-clean-css'
+import inlineCSS from 'gulp-inline-css'
+import inlineImg from 'gulp-inline-image-html'
+import uncss from 'gulp-uncss'
 import htmlmin from 'gulp-htmlmin'
 import uglify from 'gulp-uglify'
 import rimraf from 'gulp-rimraf'
@@ -21,20 +24,32 @@ gulp.task('build-js', () => gulp.src('src/**/*.js')
 
 gulp.task('build-html', () => gulp.src('src/**/*.html')
           .pipe(htmlmin({ collapseWhitespace: true, removeComments: true}))
+          .pipe(inlineCSS({
+	    applyStyleTags: false,
+	    applyLinkTags: true,
+	    removeStyleTags: false,
+	    removeLinkTags: true,
+            preserveMediaQueries: true,
+            removeHtmlSelectors: false
+          }))
+          .pipe(inlineImg('../build'))
           .pipe(gulp.dest('build'))
           .pipe(symlink('build/splash.html', {relative: true}))
 )
 
 gulp.task('build-styles', () => {
-    gulp.src('src/css/**/*.css')
-        .pipe(cleanCSS())
-        .pipe(concat('app.css'))
-        .pipe(gulp.dest('build/css'))
+  gulp.src('src/css/**/*.css')
+    .pipe(cleanCSS())
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest('build/css'))
 
-    return gulp.src('src/vendor/**/*.css')
-        .pipe(cleanCSS())
-        .pipe(concat('vendor.css'))
-        .pipe(gulp.dest('build/css'))
+  return gulp.src('src/vendor/**/*.css')
+    .pipe(uncss({
+      html: ['src/index.html']
+    }))
+    .pipe(cleanCSS())
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('build/css'))
 })
 
 gulp.task('build-static-assets', () => {
@@ -55,4 +70,4 @@ gulp.task('zip-release', () => gulp.src('build/**/*')
 
 /* npm tasks */
 
-gulp.task('build', callback => runSequence('clean-build', 'build-js', 'build-html', 'build-styles', 'build-static-assets','zip-release', callback))
+gulp.task('build', callback => runSequence('clean-build', 'build-js', 'build-static-assets','build-html','zip-release', callback))
